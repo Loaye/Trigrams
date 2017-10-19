1e39a7c04d0664ad82feff05cb3cc4c5f6a01e05
Loaye <jr.carstens00@gmail.com>
2017-10-19 14:55:14 -0700

    adds a read source function, that reads over a given text and creates a key value pair that is appeneded to a dictionary for trigrams

--- test_trigrams.py
+++ test_trigrams.py
@@ -1,5 +1,10 @@
 import pytest
 
-def test_open_file():
+def test_open_source():
     from trigrams import open_source
-    assert len(open_source('./sample.txt'))    
+    assert len(open_source('./sample.txt')) = 1
+
+def test_read_source():
+
+
+def read_source():
--- trigrams.py
+++ trigrams.py
@@ -6,26 +6,32 @@ trigrams = {}
 def main(filepath = './sample.txt', words = 200):
     """Core function that calls the rest of the algorithms"""
     source_text = open_source(filepath)
-    print(source_text)
     read_source(source_text)
+
 
 def open_source(filepath):
     """Opens the source file"""
     source_text = ''
     with io.open(filepath, encoding = 'utf-8') as source_file:
         source_text = source_file.read()
+    return source_text #return the text of the file
 
-    return source_text
-    #return the text of the file
 
 def read_source(text):
     """Reads the source file and builds trigrams"""
-
+    source_text = text.split()
+    print(source_text)
+    text_length = len(source_text)
+    for i in range(text_length - 2):
+        key = source_text[i] + ' ' + source_text[i + 1]
+        trigrams.setdefault(key, [])
+        trigrams.get(key).append(source_text[i + 2])
+    print(trigrams)
 
 def generate_text(words):
     """Writes the text from given trigram"""
 
 
 if __name__ == '__main__':
-#todo FIll this in
+    """#todo FIll this in"""
     main()
