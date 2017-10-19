7ef11a3322202b17382ced4aa5ec6fabd41a0b05
Loaye <jr.carstens00@gmail.com>
2017-10-19 13:20:25 -0700

    creates 3 basic functions that get, read, and write text based on a given source input

---
+++ trigrams.py
@@ -0,0 +1,25 @@
+"""This file contains a Trigram function that will create a Trigram on any given input"""
+
+
+trigrams = {
+
+}
+
+def main(filepath = './sample.txt', words = 200):
+    """Core function that calls the rest of the algorithms"""
+
+
+def open_source():
+    """Opens the source file"""
+
+
+def read_source():
+    """Reads the source file"""
+
+
+def generate_text():
+    """Writes the text from given trigram"""
+
+
+if __name__ == '__main__':
+#todo FIll this in 
